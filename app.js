de8617f36b4d5de27226b279063c519d5a99b477
itsik1993 <itsik810@gmail.com>
2025-06-01 16:07:48 +0300

    fix envierment verubels

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ import mongoDB from './DB/connectMongo.js';
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5179"],
+    origin: ["http://localhost:5173","http://localhost:5174",
+         ,process.env.DASHBOARD_DOMAIN
+        ,process.env.CLIENT_DOMAIN
+    ],
     credentials: true,
     optionsSuccessStatus: 200
 }));
